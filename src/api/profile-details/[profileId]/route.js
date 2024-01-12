b6d7cd4e4ca5a8611dc554e34740018038b13c16
Omkar <105555236+omkargadekar@users.noreply.github.com>
2024-01-12 18:44:38 +0530

    add one route

--- src/api/profile-details/[profileId]/route.js
+++ src/api/profile-details/[profileId]/route.js
@@ -3,7 +3,8 @@ import { connect } from "@/dbConfig/dbConfig.js";
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
-  } else if (req.method === "GET") {
-  } else if (req.method === "GET") {
+    
+  } else if (req.method === "DELETE") {
+  } else if (req.method === "PUT" || req.method === "PATCH") {
   }
 }
