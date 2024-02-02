a30f4ac09bf39995529c722d2d3ac2d025506139
klaw <netke2611@gmail.com>
2024-02-02 23:44:26 +0530

    message solved , socket pending

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,44 +1,78 @@
-// import React from 'react'
+import React, { useState } from "react";
+import Message from "./Message";
+import axios from "axios";
 
-// const ChatBox = ({ showChatbox, closeBox , friend , message }) => {
-//   return (
-//     <div class="chatbox" style={{ right: showChatbox ? "0" : "-500px" }}>
-//     <span class="comm-msg-pop-clo">
-//       <i class="fa fa-times" aria-hidden="true" onClick={closeBox}></i>
-//     </span>
+const ChatBox = ({ showChatbox, closeBox, friend, messages, userID , currentChat , currentFriend}) => {
+  const [newMessage, setNewMessage] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const message = {
+      sender: userID,
+      text: newMessage,
+      conversation: currentChat._id,
+    };
 
-//     <div class="inn">
-//       <form name="new_chat_form" method="post">
-//         <div class="s1">
-//           <img src="images/user/2.jpg" class="intephoto2" alt="" />
-//           <h4>
-//             <b class="intename2">{friend?.name}</b>,
-//           </h4>
-//           <span class="avlsta avilyes text-white">Available online</span>
-//         </div>
-//         <div class="s2 chat-box-messages">
-//           <span class="chat-wel">Start a new chat!!! now</span>
-//           <div class="chat-con">
-//             {/* <div class="chat-lhs">Hi</div>
-//             <div class="chat-rhs">Hi</div> */}
-//           </div>
-//           <span>Start A New Chat!!! Now</span>
-//         </div>
-//         <div class="s3">
-//           <input
-//             type="text"
-//             name="chat_message"
-//             placeholder="Type a message here.."
-//             required=""
-//           />
-//           <button id="chat_send1" name="chat_send" type="submit">
-//             Send <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
-//           </button>
-//         </div>
-//       </form>
-//     </div>
-//   </div>
-//   )
-// }
+    try {
+      const res = await axios.post("/api/chat/addMessage", message);
+      setAllMessages([...messages, res.data.data]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+//   const chatfriendId = friend?._id === currentChat?._id
+//   console.log(chatfriendId)
 
-// export default ChatBox
+
+  return (
+    <div className="chatbox" style={{ right: showChatbox ? "0" : "-500px" }}>
+      <span className="comm-msg-pop-clo">
+        <i className="fa fa-times" aria-hidden="true" onClick={closeBox}></i>
+      </span>
+
+      <div className="inn">
+        <form name="new_chat_form" method="post">
+          <div className="s1">
+            <img src="images/user/2.jpg" className="intephoto2" alt="" />
+            <h4>
+              <b className="intename2">
+              {friend?._id === currentChat?.members[1] ? friend?.name : "Friend's Name"}
+                </b>,
+            </h4>
+            <span className="avlsta avilyes text-white">Available online</span>
+          </div>
+          <div className="s2 chat-box-messages">
+            {/* <span className="chat-wel">Start a new chat!!! now</span> */}
+            <div className="chat-con">
+              {/* <div className="chat-lhs">Hi</div>
+        <div className="chat-rhs">Hi</div> */}
+              {messages?.map((m) => (
+                <Message message={m} own={m.sender === userID} key={m._id} />
+              ))}
+            </div>
+            {/* <span>Start A New Chat!!! Now</span> */}
+          </div>
+          <div className="s3">
+            <input
+              type="text"
+              name="chat_message"
+              placeholder="Type a message here.."
+              required=""
+              onChange={(e) => setNewMessage(e.target.value)}
+              value={newMessage}
+            />
+            <button
+              id="chat_send1"
+              name="chat_send"
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Send <i className="fa fa-paper-plane-o" aria-hidden="true"></i>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ChatBox;
--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -13,15 +13,13 @@ const Conversation = ({
   currentChat,
 }) => {
   const [friend, setFriend] = useState(null);
-  const [newMessage, setNewMessage] = useState("");
   const [allMessages, setAllMessages] = useState(null);
-  
+
   useEffect(() => {
     setAllMessages(messages);
-  },[messages]);
+  }, [messages]);
+  const friendId = conversation.members?.find((m) => m !== userID);
   useEffect(() => {
-    const friendId = conversation.members?.find((m) => m !== userID);
-
     const getFriend = async () => {
       try {
         const res = await axios.get(`/api/auth/singleuser/${friendId}`);
@@ -31,27 +29,15 @@ const Conversation = ({
       }
     };
     getFriend();
-  }, [conversation, userID]);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const message = {
-      sender: userID,
-      text: newMessage,
-      conversation: currentChat._id,
-    };
-
-    try {
-      const res = await axios.post("/api/chat/addMessage", message);
-      setAllMessages([...messages, res.data.data]);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  }, [currentChat]);
 
   return (
     <>
-      <li className="db-chat-trig" onClick={()=>handleClick(friend)} key={friend?._id}>
+      <li
+        className="db-chat-trig"
+        onClick={handleClick}
+        key={conversation?._id}
+      >
         <div className="db-chat-pro">
           {" "}
           <img src="images/profiles/1.jpg" alt="" />{" "}
@@ -68,53 +54,16 @@ const Conversation = ({
         </div>
       </li>
 
-      <div class="chatbox" style={{ right: showChatbox ? "0" : "-500px" }}>
-        <span class="comm-msg-pop-clo">
-          <i class="fa fa-times" aria-hidden="true" onClick={closeBox}></i>
-        </span>
-
-        <div class="inn">
-          <form name="new_chat_form" method="post">
-            <div class="s1">
-              <img src="images/user/2.jpg" class="intephoto2" alt="" />
-              <h4>
-                
-                <b class="intename2">{friend?.name || "No Name"}</b>,
-              </h4>
-              <span class="avlsta avilyes text-white">Available online</span>
-            </div>
-            <div class="s2 chat-box-messages">
-              {/* <span class="chat-wel">Start a new chat!!! now</span> */}
-              <div class="chat-con">
-                {/* <div class="chat-lhs">Hi</div>
-            <div class="chat-rhs">Hi</div> */}
-                {allMessages?.map((m) => (
-                  <Message message={m} own={m.sender === userID} />
-                ))}
-              </div>
-              {/* <span>Start A New Chat!!! Now</span> */}
-            </div>
-            <div class="s3">
-              <input
-                type="text"
-                name="chat_message"
-                placeholder="Type a message here.."
-                required=""
-                onChange={(e) => setNewMessage(e.target.value)}
-                value={newMessage}
-              />
-              <button
-                id="chat_send1"
-                name="chat_send"
-                type="submit"
-                onClick={handleSubmit}
-              >
-                Send <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+      {friend?._id === currentChat?.members[1] && (
+        <ChatBox
+          showChatbox={showChatbox}
+          closeBox={closeBox}
+          friend={friend}
+          messages={messages}
+          userID={userID}
+          currentChat={currentChat}
+        />
+      )}
     </>
   );
 };
